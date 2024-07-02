84ebaf8b911c1e47c85b8dfe88f80fbf82689943
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-07-02 18:40:09 +0300

    Add project files.

---
+++ 02.07/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _02._07
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
